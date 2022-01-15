544bacd0547ebd7f795daae0c6130945c602ca06
Evan McClure <evanmcclure@gmail.com>
2022-01-15 14:48:22 -0800

    Verify the command works as a kubectl plugin

--- cli_test.go
+++ cli_test.go
@@ -8,11 +8,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCLI(t *testing.T) {
-	cli := strings.Split("./kubectl-sql-query", " '")
+func TestCommand(t *testing.T) {
+	out, err := exec.Command("./kubectl-sql-query").CombinedOutput()
+
+	t.Log(string(out))
+
+	assert.NoError(t, err, "Failed to run kubectl-sql-query")
+}
+
+func TestPlugin(t *testing.T) {
+	cli := strings.Split("kubectl sql query", " ")
 	out, err := exec.Command(cli[0], cli[1:]...).CombinedOutput()
 
 	t.Log(string(out))
 
-	assert.NoError(t, err, "Failed to run kubecl-sql-query")
+	assert.NoError(t, err, "Failed to run kubectl-sql-query")
 }
