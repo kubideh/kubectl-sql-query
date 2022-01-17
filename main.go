105b7ac52257e3cff3740ad0a0d7a30f92612f5b
Evan McClure <evanmcclure@gmail.com>
2022-01-16 17:08:01 -0800

    Start testing SQL-query functionality

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/cli-runtime/pkg/printers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
-func main() {
-	flag.Usage = func() {
-		usageString := `kubectl-sql-query is the kubectl plugin to query the Kubernetes API server using SQL.
+const usageString = `kubectl-sql-query is the kubectl plugin to query the Kubernetes API server using SQL.
 
 Usage:
   kubectl sql query <query-string>
@@ -16,6 +22,9 @@ Usage:
 Flags:
   -h, --help      help for kubectl-sql-query
 `
+
+func main() {
+	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), usageString)
 	}
 
@@ -26,5 +35,53 @@ Flags:
 		os.Exit(0)
 	}
 
-	fmt.Println("No resources found in default namespace.")
+	clientConfigLoadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	configOverrides := &clientcmd.ConfigOverrides{}
+	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(clientConfigLoadingRules, configOverrides)
+	clientConfig, err := kubeConfig.ClientConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	clientSet, err := kubernetes.NewForConfig(clientConfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	streams := genericclioptions.IOStreams{
+		Out:    os.Stdout,
+		ErrOut: os.Stderr,
+	}
+
+	query(streams, clientSet, flag.Arg(0))
+}
+
+func query(streams genericclioptions.IOStreams, clientSet kubernetes.Interface, sqlQuery string) {
+	printer := printers.NewTablePrinter(printers.PrintOptions{})
+
+	namespace := ""
+	if strings.Contains(sqlQuery, "namespace=default") {
+		namespace = "default"
+	} else {
+		namespace = "kube-system"
+	}
+
+	if strings.Contains(sqlQuery, "name=") {
+		pod, err := clientSet.CoreV1().Pods(namespace).Get(context.TODO(), "kube-apiserver-kind-control-plane", metav1.GetOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		printer.PrintObj(pod, streams.Out)
+	} else {
+		pods, err := clientSet.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		if len(pods.Items) == 0 {
+			fmt.Fprintln(streams.ErrOut, "No resources found in default namespace.")
+		} else {
+			printer.PrintObj(pods, streams.Out)
+		}
+	}
 }
---
+++ query_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+// one pod in a namespace
+// no pods in a namespace
+// multiple pods in a namespace
+
+func TestQueryOnePodWithResult(t *testing.T) {
+	fakeClientSet := fake.NewSimpleClientset()
+	fakeClientSet.CoreV1().Pods("kube-system").Create(
+		context.TODO(),
+		&v1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "kube-system",
+				Name:      "kube-apiserver-kind-control-plane",
+			},
+		},
+		metav1.CreateOptions{},
+	)
+
+	streams, _, outBuf, errBuf := genericclioptions.NewTestIOStreams()
+
+	query(streams, fakeClientSet, "SELECT * FROM pods WHERE name=kube-apiserver-kind-control-plane AND namespace=kube-system")
+
+	const expectedOutput = "NAME                                AGE\nkube-apiserver-kind-control-plane   <unknown>\n"
+	assert.Equal(t, expectedOutput, outBuf.String())
+	assert.Equal(t, "", errBuf.String())
+}
+
+func TestQueryAllPodsInDefaultNamespaceWithNoResults(t *testing.T) {
+	fakeClientSet := fake.NewSimpleClientset()
+
+	streams, _, outBuf, errBuf := genericclioptions.NewTestIOStreams()
+
+	query(streams, fakeClientSet, "SELECT * FROM pods WHERE namespace=default")
+
+	assert.Equal(t, "", outBuf.String())
+	assert.Equal(t, "No resources found in default namespace.\n", errBuf.String())
+}
